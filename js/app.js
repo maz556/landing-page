ccb9deef70040467b19d464375eea7273608d357
Manuel Zamora <mzamora@sparkcognition.com>
2020-07-23 13:26:40 -0500

    Addition of nav activation and improvements

--- js/app.js
+++ js/app.js
@@ -13,22 +13,20 @@
  * 
 */
 
-let sections;
-let nav_sect = {};
+let nav_sect = new Map();
 
 // Builds the nav bar dynamically based on page content
 function build_nav() {
     // Find element from each section of the page
-    sections = document.getElementsByTagName("section");
+    const sections = document.getElementsByTagName("section");
     const frag = document.createDocumentFragment();
     // Loop through sections to create nav bar element for each one
     for (var section of sections) {
         const list_item = document.createElement("li");
         const text = section.querySelector("h2").textContent;
         list_item.textContent = text;
-        list_item.id = `${section.id}-nav`;
         list_item.classList.add("menu__link")
-        nav_sect[list_item.id] = section;
+        nav_sect.set(list_item, section);
         frag.appendChild(list_item);
     }
     // Append newly created document fragment to the navbar
@@ -38,14 +36,16 @@ function build_nav() {
     nav_list.addEventListener("click", click_scroll)
 }
 
-// Adds/removes active class for each section depending on viewport
+// Adds/removes active class for each section/navigation item depending on viewport
 function set_actives() {
-    for (var section of sections) {
-        const rect = section.getBoundingClientRect();
+    for (var [nav, sect] of nav_sect) {
+        const rect = sect.getBoundingClientRect();
         if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-            section.classList.add("your-active-class");
+            nav.classList.add("your-active-class");
+            sect.classList.add("your-active-class");
         } else {
-            section.classList.remove("your-active-class");
+            nav.classList.remove("your-active-class");
+            sect.classList.remove("your-active-class");
         }
     }
 }
@@ -54,7 +54,7 @@ function set_actives() {
 function click_scroll(evt) {
     const el = evt.target;
     if (!el.classList.contains("menu__link")) return;
-    const sect = nav_sect[el.id];
+    const sect = nav_sect.get(el);
     sect.scrollIntoView({behavior: "smooth"});
 }
 
@@ -63,4 +63,3 @@ document.addEventListener("DOMContentLoaded", build_nav)
 
 // Set active section on scroll
 document.addEventListener("scroll", set_actives)
-
